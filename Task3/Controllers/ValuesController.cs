5ef0026b1621017078005caf4f011ae166059690
PixlRainbow <yongzhengleow.17@ichat.sp.edu.sg>
2019-06-24 23:29:42 +0800

    Workaround: manually query username

--- Task3/Controllers/ValuesController.cs
+++ Task3/Controllers/ValuesController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSCAssignment.Controllers
@@ -14,12 +15,29 @@ namespace CSCAssignment.Controllers
     [Route("/api/[controller]")]
     public class ValuesController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ValuesController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
         // GET api/values
         [HttpGet]
         public string Get()
         {
-            var userName = this.User.Identity.Name;
-            return String.Format("Hello, {0}.", userName);
+            var sub = this.User.Claims
+                .Where(c => c.Type.Equals("sub"))
+                .FirstOrDefault();
+            if(sub != null){
+                string userId = sub.Value;
+                var user = _userManager.Users.Where(u => u.Id.Equals(userId)).FirstOrDefault();
+                if(user != null){
+                    string userName = user.UserName;
+                    return String.Format("Hello, {0}.", userName);
+                }
+                return "cannot find user with given id";
+            }
+            return "no user id associated with token";
         }
     }
 }
